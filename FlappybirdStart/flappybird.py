20742a19702e0cdee88cb3f58a2a68bb1f153e93
Zeejai Leonard <793313@seq.org>
2019-07-08 09:27:34 -0700

    made flappybird starter code bare bones

--- FlappybirdStart/flappybird.py
+++ FlappybirdStart/flappybird.py
@@ -1,6 +1,4 @@
-
 import random
-
 
 TITLE = 'Flappy Bird'
 WIDTH = 400
@@ -9,80 +7,23 @@ HEIGHT = 708
 # These constants control the difficulty of the game
 
 
-
 def reset_pipes():
     pass
 
-
-pipe_top = Actor('top', anchor=('left', 'bottom'))
-pipe_bottom = Actor('bottom', anchor=('left', 'top'))
-reset_pipes()  # Set initial pipe positions.
+# Set initial pipe positions.
 
 
 def update_pipes():
-    pipe_top.left -= SPEED
-    pipe_bottom.left -= SPEED
-    if pipe_top.right < 0:
-        reset_pipes()
-        if not bird.dead:
-            bird.score += 1
-            if bird.score > storage['highscore']:
-                storage['highscore'] = bird.score
-
+    pass
 
 def update_bird():
-    uy = bird.vy
-    bird.vy += GRAVITY
-    bird.y += (uy + bird.vy) / 2
-    bird.x = 75
-
-    if not bird.dead:
-        if bird.vy < -3:
-            bird.image = 'bird2'
-        else:
-            bird.image = 'bird1'
-
-    if bird.colliderect(pipe_top) or bird.colliderect(pipe_bottom):
-        bird.dead = True
-        bird.image = 'birddead'
-
-    if not 0 < bird.y < 720:
-        bird.y = 200
-        bird.dead = False
-        bird.score = 0
-        bird.vy = 0
-        reset_pipes()
-
+    pass
 
 def update():
-    update_pipes()
-    update_bird()
-
+    pass
 
 def on_key_down():
-    if not bird.dead:
-        bird.vy = -FLAP_STRENGTH
-
+    pass
 
 def draw():
-    screen.blit('background', (0, 0))
-    pipe_top.draw()
-    pipe_bottom.draw()
-    bird.draw()
-    screen.draw.text(
-        str(bird.score),
-        color='white',
-        midtop=(WIDTH // 2, 10),
-        fontsize=70,
-        shadow=(1, 1)
-    )
-    screen.draw.text(
-        "Best: {}".format(storage['highscore']),
-        color=(200, 170, 0),
-        midbottom=(WIDTH // 2, HEIGHT - 10),
-        fontsize=30,
-        shadow=(1, 1)
-    )
-
-
-pgzrun.go()
+    pass
